c70b27688b30767f77a1ea26cc57cf43b5311843
SPsuyash <144734900+SPsuyash@users.noreply.github.com>
2024-02-12 18:43:14 +0530

    Time: 12 ms (25.38%), Space: 9.8 MB (40.04%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int normal[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        string res;
-        for(int i=0;i<13;i++){
-            while(num>=normal[i]){
-                res.append(roman[i]);
-                num-=normal[i];
-            }
-        }
-        return res;
+        string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string thousands[]= {"", "M", "MM", "MMM"};
+        string Roman =  thousands[num / 1000] + hundreds[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+        return Roman;
     }
 };
