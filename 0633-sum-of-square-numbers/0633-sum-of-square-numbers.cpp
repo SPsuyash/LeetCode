28b500f8368971eba15a06d1806333400c82e527
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2024-06-17 23:38:27 +0530

    Time: 0 ms (100%), Space: 7.2 MB (18.45%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(double a=0 ; a*a<=c ;a++)
+        {
+            double b=sqrt(c-a*a);
+            if(b==int(b))
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
