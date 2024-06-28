25565376cfa74b2eb9b3cf502ef57351ef51997f
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2024-06-29 01:46:51 +0530

    Time: 418 ms (56.49%), Space: 11.3 MB (92.20%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int countPrimes(int n) {
-       
+
         if (n <= 2) {
             return 0;
         }
-        
+
         vector<bool> ans(n, false);
         int limit = sqrt(n);
-        
+
         for (int i = 2; i <= limit; i++) {
             if (ans[i] == false) {
                 for (int j = i * i; j < n; j += i) {
@@ -16,15 +16,14 @@ public:
                 }
             }
         }
-        
+
         int c = 0;
         for (int i = 2; i < n; i++) {
             if (ans[i] == false) {
                 c++;
             }
         }
-        
+
         return c;
-}
-    
+    }
 };
