86be0a28128a0d523863ccd2075ca781f9b60dbf
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2024-07-10 19:59:01 +0530

    Time: 3 ms (43.79%), Space: 7.2 MB (69.74%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -3,11 +3,13 @@ public:
     int minBitFlips(int start, int goal) {
         int c=0;
         int ans=start^goal;
-     
-            while(ans)
+        for(int i=0;i<31;i++)
+        {
+            if(ans)
             {
                 c+=ans&1;
                 ans>>=1;
+            }
         }
         return c;
     }
