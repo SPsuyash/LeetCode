59fdd429f7c19a60d1d29cfc56a34c1d3043660e
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2024-10-03 23:53:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (22.48%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,80 +1,68 @@
 class MyQueue {
 public:
-    stack<int>st;
-    stack<int>helper;
+ stack<int>st1;
+ stack<int>st2;
     MyQueue() {
-        
+       
     }
     
     void push(int x) {
-        if(st.size()==0) {
-            st.push(x);
-            return;
-        }
-        else {
-            while(st.size()>0){
-                helper.push(st.top());
-                st.pop();
-            }
-            st.push(x);
-            while(helper.size()>0){
-                st.push(helper.top());
-                helper.pop();
-            }
-        }
+        st1.push(x);
     }
     
     int pop() {
-        int x=st.top();
-        st.pop();
-        return x;
+        if(empty())
+        {
+            return 0;
+        }
+        if(!st2.empty())
+        {
+            int element=st2.top();
+            st2.pop();
+            return element;
+        }
+        else{
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                st1.pop();
+            }
+            int element=st2.top();
+            st2.pop();
+            return element;
+        }
+        
     }
     
-    int peek() { 
-        return st.top();
+    int peek() {
+        if(empty())
+        {
+            return 0;
+        }
+        if(!st2.empty())
+        {
+            return st2.top();
+        }
+        else {
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                    st1.pop();                
+            }
+            return st2.top();
+        }
     }
     
-    bool empty() {  
-       if(st.size()==0) return true;
-       else return false; 
+    bool empty() {
+        return st1.empty() && st2.empty();
     }
 };
-   
-    stack<int>st;
-    stack<int>helper;
-    
-    void push(int x) { 
-        st.push(x);
-    }
-    
-    int pop() { 
-        while(st.size()>0){
-            helper.push(st.top());
-            st.pop();
-        }
-        int x=helper.top();
-        helper.pop();
-        while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop();
-        }
-        return x;
-    }
-    
-    int peek() { 
-        while(st.size()>0){
-            helper.push(st.top());
-            st.pop();
-        }
-        int x=helper.top();
-        while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop();
-        }
-        return x;
-    }
-    
-    bool empty() {  
-       if(st.size()==0) return true;
-       else return false; 
-    }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
