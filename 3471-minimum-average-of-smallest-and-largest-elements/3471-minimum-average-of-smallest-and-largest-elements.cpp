a3e3099c8ec3ba6c29198bf4ca61fa79a44fe5cd
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2024-11-27 22:35:00 +0530

    Time: 0 ms (100%), Space: 25.8 MB (7.8%) - LeetHub

---
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+        vector<double>ans;
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size()-1;
+        double avg;
+        while(i<=j)
+        {
+            avg=(nums[i]+nums[j])/2.0;
+            ans.push_back(avg);
+            i++,j--;
+        }
+        double min=DBL_MAX;
+        for(int i=0;i<ans.size();i++)
+        {
+            if(min>ans[i])
+            {
+                min=ans[i];
+            }
+        }
+        return min;
+    }
+};
