67d0d118dc67a448d9afb2653843f28c292889e8
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2025-04-13 15:12:15 +0530

    Time: 0 ms (100%), Space: 8 MB (34.93%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -3,26 +3,22 @@ public:
     const int MOD = 1e9 + 7;
 
     long long modPow(long long base, long long exp, int mod) {
-        long long result = 1;
-        base %= mod;
-
-        while (exp > 0) {
-            if (exp % 2 == 1)
-                result = (result * base) % mod;
-            base = (base * base) % mod;
-            exp /= 2;
+        if(exp==0)
+        {
+            return 1;
         }
-
+        long long half=modPow(base,exp/2,mod);
+        long long result=(half*half)%mod;
+        if(exp%2==1)
+        {
+            result=(result*base)%mod;
+        }
         return result;
     }
 
     int countGoodNumbers(long long n) {
-        long long evenPositions = (n + 1) / 2;
-        long long oddPositions = n / 2;
-
-        long long evenCount = modPow(5, evenPositions, MOD);
-        long long oddCount = modPow(4, oddPositions, MOD);
-
-        return (evenCount * oddCount) % MOD;
+      long long even = modPow(5, (n + 1) / 2, MOD);
+        long long odd = modPow(4, n / 2, MOD);
+        return (even * odd) % MOD;
     }
 };
