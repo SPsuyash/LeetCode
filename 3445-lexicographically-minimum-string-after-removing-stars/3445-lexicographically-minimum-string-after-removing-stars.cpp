b07048dffde18e89198ff0750c0bf2245d152a81
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2025-06-07 13:01:09 +0530

    Time: 26 ms (96.1%), Space: 25.9 MB (78.9%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        vector<int> buckets[26];
+        int mask = 0;
+        vector<char> res;
+        res.reserve(s.size());
+        for (char ch : s) {
+            if (ch != '*') {
+                int i = ch - 'a';
+                buckets[i].push_back(res.size());
+                mask |= 1 << i;
+                res.push_back(ch);
+            } else {
+                int lowbit = mask & -mask;
+                int i = __builtin_ctz(lowbit);
+                int pos = buckets[i].back();
+                buckets[i].pop_back();
+                if (buckets[i].empty()) mask ^= lowbit;
+                res[pos] = 0;
+            }
+        }
+        string ans;
+        ans.reserve(res.size());
+        for (char c : res) if (c) ans.push_back(c);
+        return ans;
+    }
+};
