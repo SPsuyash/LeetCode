fa152656b21ba13ae55f835d6e5978abc7d2109e
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2025-07-11 14:08:46 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+//bottom up approach
+int solveTab(vector<int>& nums,int n)
+{
+    vector<int> dp(n+2,0);
+    dp[nums.size()]=0;
+
+    for(int i=n-1;i>=0;i--)
+    {
+        int include=nums[i]+dp[i+2];
+        int exclude=0+dp[i+1];
+        dp[i]=max(include,exclude);
+    }
+    return dp[0];
+}
+//top down approach
 int solveMem(vector<int>& nums,int i,vector<int>& dp){
     if(i>=nums.size())
     {
@@ -16,8 +31,9 @@ int solveMem(vector<int>& nums,int i,vector<int>& dp){
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        int i=0;
-       return solveMem(nums,i,dp);
+    //     vector<int>dp(n+1,-1);
+    //     int i=0;
+    //    return solveMem(nums,i,dp);
+    return solveTab(nums,n);
     }
 };
