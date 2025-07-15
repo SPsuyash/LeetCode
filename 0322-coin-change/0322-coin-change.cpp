4e9ea0e2822146b0692753f1b2e14cf21ccd2e71
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2025-07-15 12:32:49 +0530

    Time: 16 ms (89.12%), Space: 22.2 MB (51.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -20,33 +20,68 @@ public:
 //         }
 //     }
 //     return mini;
+// }
+
+// top down
+// int solveMem(vector<int>&coins,int amount,vector<int>&dp)
+// {
+//     if(amount==0)
+//     {
+//         return 0;
+//     }
+//     if(dp[amount]!=-1)
+//     {
+//         return dp[amount];
+//     }
+//     int mini=INT_MAX;
+//     for(int i=0;i<coins.size();i++)
+//     {
+//         int coin=coins[i];
+//         if(coin<=amount)
+//         {
+//             int recursive=solveMem(coins,amount-coin,dp);
+//             if(recursive!=INT_MAX){
+//                 int coinsUsed=1+recursive;
+//                 mini=min(mini,coinsUsed);
+//             }
+//         }
+//     }
+//     dp[amount]=mini;
+//     return dp[amount];
 // } 
-int solveMem(vector<int>&coins,int amount,vector<int>&dp)
-{
-    if(amount==0)
+
+
+//bottom up
+
+int solveTab(vector<int>& coins, int amount){
+    //create arr
+    vector<int>dp(amount+1,-1);
+    //basecase
+    dp[0]=0;
+    //range n to amount 0 in top down then we have to reverse
+
+    for(int amt=1;amt<=amount;amt++)
     {
-        return 0;
-    }
-    if(dp[amount]!=-1)
-    {
-        return dp[amount];
-    }
-    int mini=INT_MAX;
+         //logic
+               int mini=INT_MAX;
     for(int i=0;i<coins.size();i++)
     {
         int coin=coins[i];
-        if(coin<=amount)
+        if(coin<=amt)
         {
-            int recursive=solveMem(coins,amount-coin,dp);
+            int recursive=dp[amt-coin];
             if(recursive!=INT_MAX){
                 int coinsUsed=1+recursive;
                 mini=min(mini,coinsUsed);
             }
         }
     }
-    dp[amount]=mini;
+    dp[amt]=mini; 
+    }
     return dp[amount];
-} 
+}
+
+
     int coinChange(vector<int>& coins, int amount) {
         // int ans=solve(coins,amount);
         // if(ans==INT_MAX)
@@ -54,8 +89,16 @@ int solveMem(vector<int>&coins,int amount,vector<int>&dp)
         //     return -1;
         // }
         // return ans;
-        vector<int>dp(amount+1,-1);
-        int ans=solveMem(coins,amount,dp);
+
+
+        // vector<int>dp(amount+1,-1);
+        // int ans=solveMem(coins,amount,dp);
+        // if(ans==INT_MAX)
+        // {
+        //     return -1;
+        // }
+         vector<int>dp(amount+1,-1);
+        int ans=solveTab(coins,amount);
         if(ans==INT_MAX)
         {
             return -1;
