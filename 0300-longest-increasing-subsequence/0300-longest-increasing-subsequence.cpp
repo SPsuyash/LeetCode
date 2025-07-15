a90d27f252546dc76be82e24001411e88f35b2a1
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2025-07-15 12:53:56 +0530

    Time: 661 ms (5.01%), Space: 296.6 MB (13.28%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int last, int i, vector<vector<int>>& dp) {
-        if (i >= nums.size()) return 0;
-        if (dp[last+1][i] != -1) 
-       {
-         return dp[last+1][i];
-         }
+// int solveRec(vector<int>&nums,int curr,int prev)
+// {
+//     if(curr>=nums.size())
+//     {
+//         return 0;
+//     }
+//     int inc=0;
+//     if(prev==-1 || nums[prev]<nums[curr])
+//     {
+//         inc=1+solveRec(nums,curr+1,curr);
+//     }
+//     int exc=0+solveRec(nums,curr+1,prev);
+//     int final=max(inc,exc);
+//     return final;
+// }
 
-        int inc = 0;
-        if (last == -1 || nums[i] > nums[last]) {
-            inc = 1 + solve(nums, i, i + 1, dp);
-        }
-        int exc = solve(nums, last, i + 1, dp);
+//2d
 
-        dp[last+1][i] = max(inc, exc);
-        return dp[last+1][i];
+int solveMem(vector<int>&nums,int curr,int prev,vector<vector<int>>&dp)
+{
+    if(curr>=nums.size())
+    {
+        return 0;
     }
-
+    if(dp[curr][prev+1]!=-1)
+    {
+        return dp[curr][prev+1];
+    }
+    int inc=0;
+    if(prev==-1 || nums[prev]<nums[curr])
+    {
+        inc=1+solveMem(nums,curr+1,curr,dp);
+    }
+    int exc=0+solveMem(nums,curr+1,prev,dp);
+    dp[curr][prev+1]=max(inc,exc);
+    return dp[curr][prev+1];
+}
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 2, -1));
-        return solve(nums, -1, 0, dp);
+        int curr=0;
+        int prev=-1;
+        int n=nums.size();
+        // return solveRec(nums,curr,prev); 
+        //observation prev alawat back at curr  
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solveMem(nums,curr,prev,dp);
     }
 };
