8b14ec5ac652acd822ce13db1b92ac2b2f76a63f
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2025-08-09 16:14:06 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,57 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-       //sort(nums.begin(),nums.end());
-    //    int z,o,t;
-    //    z=o=t=0;
-    //    for(int i=0;i<nums.size();i++)
-    //    {
-    //        if(nums[i]==0)
-    //        {
-    //            z++;
-    //        }
-    //        else if(nums[i]==1)
-    //        {
-    //            o++;
-    //        }
-    //        else
-    //        {
-    //            t++;
-    //        }
-    //    }
-    //    int i=0;
-    //    while(z--)
-    //    {
-    //        nums[i]=0;
-    //        i++;
-    //        }
-    //        while(o--)
-    //    {
-    //        nums[i]=1;
-    //        i++;
-    //        }
-    //        while(t--)
-    //    {
-    //        nums[i]=2;
-    //        i++;
-    //        }
-    int l=0,m=0,h=nums.size()-1;
-    while(m<=h)
-    {
-        if(nums[m]==0)
-        {
-            swap(nums[l],nums[m]);
-            l++,m++;
+        int n=nums.size()-1;
+        int a=0,b=0;
+        while(b<=n){
+            if(nums[b]==0)
+            {
+                swap(nums[a],nums[b]);
+                a++;
+                b++;
+            }
+            else if(nums[b]==1)
+            {
+                b++;
+            }
+            else{
+                swap(nums[b],nums[n]);
+                n--;
+              
+            }
         }
-        else if(nums[m]==1)
-        {
-            m++;
-        }
-        else
-        {
-            swap(nums[m],nums[h]);
-            h--;
-        }
+        
     }
-           }
 };
