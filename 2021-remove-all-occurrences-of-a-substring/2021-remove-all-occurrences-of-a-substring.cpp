9fe65de1cc2de5a30df4b068805738aa8af0a082
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2025-08-14 23:32:02 +0530

    Time: 3 ms (33.63%), Space: 12.8 MB (5.07%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+void strremove(string& s,string& part)
+{
+    int n=s.find(part);
+    if(n!=string::npos){
+       string left = s.substr(0,n);
+       string right= s.substr(n+part.size(),s.size());
+       s=left+right;
+       strremove(s,part);
+    }
+    else{
+        return;
+    }
+}
     string removeOccurrences(string s, string part) {
-        int pos = s.find(part);
-        while(pos != string::npos) {
-            s.erase(pos , part.length());
-            pos=s.find(part);
-        }
+        strremove(s,part);
         return s;
-        // chappa nhi hai professnal tarike se kiiya hai
+    
     }
 };
