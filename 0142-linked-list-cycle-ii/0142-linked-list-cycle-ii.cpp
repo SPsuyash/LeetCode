d8f003d4c93661ea73220a29dbfb7b112468f47e
SUYASH PATEL <144734900+SPsuyash@users.noreply.github.com>
2025-08-16 20:03:27 +0530

    Time: 10 ms (46.4%), Space: 11.4 MB (24.12%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -11,21 +11,21 @@ public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* slow=head;
         ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL)
+        while(fast!=nullptr && fast->next!=nullptr)
         {
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast)
+         slow=slow->next;
+         fast=fast->next->next;
+         if(slow==fast)
+         {
+            slow=head;
+            while(slow!=fast)
             {
-              slow=head;
-              while(slow!=fast)
-              {
                 slow=slow->next;
                 fast=fast->next;
-              }
-              return slow; 
             }
-    }
-    return NULL;
+            return slow;
+         }
+        }
+        return NULL;
     }
 };
